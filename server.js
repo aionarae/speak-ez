3ac8c84b527a65e4d40dd44ed81a15a817f4ae70
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-06-11 21:32:50 -0600

    updated server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const sess = {
   })
 };
 
+app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
@@ -38,11 +39,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Login route
-app.get('/login', (req, res) => {
-  res.render('login');
-});
 
 app.use(routes);
 
