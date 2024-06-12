c83ad8dd65af084aa3a86fdae1653a4cee78e775
Aiona <aionarae@users.noreply.github.com>
2024-06-11 19:29:45 -0600

    added homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,4 +4,16 @@ const withAuth = require('../utils/auth');
 
 //add routes here
 
+router.get('/', async (req, res) => {
+  try {
+    // Add any logic you need here
+
+    res.render('homepage', {
+      loggedIn: req.session.loggedIn
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
