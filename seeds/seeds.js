b95962f15cd99cc6b0eee6ee034a363cd3155ffb
edmundseric13 <160555371+edmundseric13@users.noreply.github.com>
2024-06-13 19:43:53 -0600

    reverted seeds file

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,6 +1,5 @@
 const sequelize = require('../config/connection');
 const { User, Role, Service } = require('../models');
-const bcrypt = require('bcrypt');
 
 const userData = require('./userData.json');
 const roleData = require('./roleData.json');
@@ -10,27 +9,23 @@ const seedDatabase = async () => {
   try {
     await sequelize.sync({ force: true });
 
-    const saltRounds = 10;
-    const hashedUserData = await Promise.all(
-      userData.map(async (user) => {
-        const hashedPassword = await bcrypt.hash(user.password, saltRounds);
-        return { ...user, password: hashedPassword };
-      })
-    );
-
-    const users = await User.bulkCreate(hashedUserData, {
+    let users = await User.bulkCreate(userData, {
       individualHooks: true,
-      returning: true,
+      returning: true
     });
 
-    for (const serviceData of serviceData) {
-      const randomUser = users[Math.floor(Math.random() * users.length)];
-      await Service.create({ ...serviceData, user_id: randomUser.id });
+    for (const service of serviceData) {
+      await Service.create({
+        ...service,
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+      });
     }
 
-    for (const roleData of roleData) {
-      const randomUser = users[Math.floor(Math.random() * users.length)];
-      await Role.create({ ...roleData, user_id: randomUser.id });
+    for (const role of roleData) {
+      await Role.create({
+        ...role,
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+      });
     }
 
     process.exit(0);
