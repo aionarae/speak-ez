e28bdea88f2dc7b1267e4d110667e36810238173
Aiona <aionarae@users.noreply.github.com>
2024-06-16 18:47:57 -0600

    fixed controllers

--- controllers/api/serviceRoutes.js
+++ controllers/api/serviceRoutes.js
@@ -7,21 +7,31 @@ const {
   getServiceById,
   updateService,
   deleteService,
-} = require('../serviceController');
+} = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // Create a new service
-router.post('/', createService);
+// router.post('/', createService);
 
 // Get all services
-router.get('/', getAllServices);
+// router.get('/', getAllServices) ;
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const serviceData = await Service.findAll();
+    res.status(200).json(serviceData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 
 // Get a single service by ID
-router.get('/:id', getServiceById);
+// router.get('/:id', getServiceById);
 
 // Update a service
-router.put('/:id', updateService);
+// router.put('/:id', updateService);
 
 // Delete a service
-router.delete('/:id', deleteService);
+// router.delete('/:id', deleteService);
 
 module.exports = router;
