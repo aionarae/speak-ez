69d2fc15fbf8efb8b4d5dac2656cb217804a28bb
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-06-17 12:45:49 -0600

    fixed routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
 const { User, Role, Service } = require('../models');
 const withAuth = require('../utils/auth');
-
-//add routes here
+const serviceData = require('../seeds/serviceData.json');
 
 router.get('/', async (req, res) => {
   try {
-    // Add any logic you need here
     res.render('homepage', {
       loggedIn: req.session.loggedIn
     });
@@ -17,7 +15,6 @@ router.get('/', async (req, res) => {
 
 router.get('/signup', async (req, res) => {
   try {
-    // Add any logic you need here
     res.render('signup');
   } catch (err) {
     res.status(500).json(err);
@@ -26,7 +23,6 @@ router.get('/signup', async (req, res) => {
 
 router.get('/login', async (req, res) => {
   try {
-    // If user is logged in, redirect to 
     if (req.session.loggedIn) {
       res.redirect('/services');
       return;
@@ -37,5 +33,67 @@ router.get('/login', async (req, res) => {
   }
 });
 
+router.get('/services', withAuth, async (req, res) => {
+  try {
+    res.render('services_list', {
+      services: serviceData,
+      loggedIn: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+router.post('/schedule', withAuth, async (req, res) => {
+  try {
+    const { service, time } = req.body;
+    
+    // Parse the selected time
+    const selectedTime = new Date(time);
+
+    // Find the service by name
+    const serviceData = await Service.findOne({ where: { name: service } });
+
+    if (!serviceData) {
+      res.status(404).json({ message: 'Service not found' });
+      return;
+    }
+
+    // Create a new scheduled service associated with the user
+    await Service.create({
+      name: serviceData.name,
+      description: serviceData.description,
+      price: serviceData.price,
+      duration: serviceData.duration,
+      date: selectedTime.toISOString().split('T')[0], // Extract the date portion
+      time: selectedTime.toISOString().split('T')[1].slice(0, 8), // Extract the time portion
+      status: 'pending',
+      user_id: req.session.user_id,
+    });
+
+    res.redirect('/user-services');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/user-services', withAuth, async (req, res) => {
+  try {
+    const userServices = await Service.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+
+    res.render('user_services_list', {
+      userServices: userServices.map((service) => service.get({ plain: true })),
+      loggedIn: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
